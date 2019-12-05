ead8b51a1c18e9a78447e5186e2f62f2ead060ee
Olaf Willner <olafdrw@gmail.com>
2019-12-05 21:27:27 +0000

    update

---
+++ script 2.js
@@ -0,0 +1,11 @@
+// $(window).scroll(function() {
+//         var ScrollTop = parseInt($(window).scrollTop());
+//          //console.log(scroll);
+//         if (ScrollTop > 150) {
+//             //console.log('ScrollTop');
+//             $(".header-inner").addClass("header");
+//         } else {
+//             //console.log('ScrollTop ');
+//             $(".header-inner").removeClass("header");
+//         }
+// });
